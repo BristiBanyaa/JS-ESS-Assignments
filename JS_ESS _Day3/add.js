180338af061308f8ec2cc92d3b5ff4203a2fdfd9
BristiBanyaa <86700289+BristiBanyaa@users.noreply.github.com>
2021-08-05 07:34:09 -0700

    JS ESS Assignment Day 3 Submitted.

---
+++ JS_ESS _Day3/add.js
@@ -0,0 +1,8 @@
+
+function add()
+
+{
+let name=document.querySelector("#Name").value;
+let city=document.querySelector("#city").value;
+document.querySelector ("#result").innerHTML=`Name: ${name} City:${city}`;
+}
